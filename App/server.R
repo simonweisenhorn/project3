78d904c13f45a4190a3d3c64ed261da21f7c42cc
simonweisenhorn <simonweisenhorn@gmail.com>
2023-07-27 22:56:27 -0400

    Major work done on the model fitting tab on the modeling page. Data csv was also added to the project.

--- App/server.R
+++ App/server.R
@@ -1,16 +1,99 @@
 
 library(shiny)
+library(tidyverse)
 
 
-function(input, output, session) {
+shinyServer(function(input, output, session) {
   
   
   
-  url <- a("Kaggle", href="https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/overview")
+  url <- a("Kaggle", href="https://www.kaggle.com/datasets/ashydv/housing-dataset")
   
   output$tab <- renderUI({
     tagList("Data Source:", url)
   })
   
-}
+  
+  getData <- reactive({
+    newData <- read_csv("../Housing.csv")
+  })
+  
+  
+  observeEvent(input$trainSplit, {
+    change <- 1-input$trainSplit
+    updateNumericInput(session, "testSplit", value=change)
+  })
+  
+  observeEvent(input$testSplit, {
+    change <- 1-input$testSplit
+    updateNumericInput(session, "trainSplit", value=change)
+    })
+  
+  output$table <- renderTable({
+    getData()
+  })
+  
+  observeEvent(input$runModels, {
+    
+    newData <- getData()
+    
+    newData$mainroad <- as.factor(newData$mainroad)
+    newData$guestroom <- as.factor(newData$guestroom)
+    newData$basement <- as.factor(newData$basement)
+    newData$hotwaterheating <- as.factor(newData$hotwaterheating)
+    newData$airconditioning <- as.factor(newData$airconditioning)
+    newData$prefarea <- as.factor(newData$prefarea)
+    newData$furnishingstatus <- as.factor(newData$furnishingstatus)
+    
+    
+    
+    trainSplit <- input$trainSplit
+    splitIndexes <- createDataPartition(newData$price, p=trainSplit, list=FALSE)
+    dataTrain <- newData[splitIndexes, ]
+    dataTest <- newData[-splitIndexes, ]
+    
+    cvMlrNumber <- input$mlrCvFold
+
+    if ((input$mlrInteractions == 0) & (input$mlrVariables == "all")) {
+      mlrFit <- train(price ~ ., data = dataTrain,
+                    method = "lm",
+                    preProcess = c("center", "scale"),
+                    trControl = trainControl(method = "cv", number = cvMlrNumber))
+    } else if ((input$mlrInteractions == 1) & (input$mlrVariables == "all")){
+      mlrFit <- train(price ~ .^2, data = dataTrain,
+                    method = "lm",
+                    preProcess = c("center", "scale"),
+                    trControl = trainControl(method = "cv", number = cvMlrNumber))
+    }
+    
+    
+    
+
+    if ((input$mlrInteractions == 0) & (input$mlrVariables == "custom")) {
+      
+      my.formula <- paste("price", '~', paste(input$mlrCustom, collapse = "+") )
+      my.formula <- as.formula(my.formula)
+      
+      mlrFit <- train(my.formula, data = dataTrain,
+                      method = "lm",
+                      preProcess = c("center", "scale"),
+                      trControl = trainControl(method = "cv", number = cvMlrNumber))
+      
+    } else if((input$mlrInteractions == 1) & (input$mlrVariables == "custom")){
+      
+      my.formula <- paste("price", '~', "(", paste(input$mlrCustom, collapse = "+"), ")^2")
+      my.formula <- as.formula(my.formula)
+      
+      mlrFit <- train(my.formula, data = dataTrain,
+                      method = "lm",
+                      preProcess = c("center", "scale"),
+                      trControl = trainControl(method = "cv", number = cvMlrNumber))
+    }
+    
+    output$modelSummary <- renderPrint(summary(mlrFit))
+    
+  })
+  
+  
+})
 
--- App/ui.R
+++ App/ui.R
@@ -1,8 +1,11 @@
 
 
 library(shiny)
+library(shinythemes)
 
-shinyUI(navbarPage("Navigation Tabs",
+
+fluidPage(theme = shinytheme("cerulean"),
+        navbarPage("Navigation Tabs",
                    tabPanel("About",
                             mainPanel(
                                 h1("Welcome to my Housing App!", align="center"),
@@ -37,14 +40,133 @@ shinyUI(navbarPage("Navigation Tabs",
                             )
                    ),
                    tabPanel("Modeling",
-                            sidebarLayout(
-                              sidebarPanel(
+                            tabsetPanel(
+                              tabPanel("Modeling Info",
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           
+                                         ),
+                                         mainPanel(
                                 
+                                         )
+                                       )
                               ),
-                              mainPanel(
-                                
+                              tabPanel("Model Fitting",
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           numericInput("trainSplit",
+                                                  label="Select Training Split",
+                                                  value=.75, min=0, max=1,
+                                                  step=.05),
+                                           numericInput("testSplit",
+                                                  label="Select Testing Split",
+                                                  value=.25, min=0, max=1,
+                                                  step=.05),
+                                           h4(strong("Multiple Linear Regression Model"), style = "color:red;"),
+                                           radioButtons("mlrVariables", 
+                                                              "What Variables Should be Included?",
+                                                              c("All Variables" = "all",
+                                                                "Custom Subset" = "custom")),
+                                           conditionalPanel(condition = "input.mlrVariables.includes('custom')",
+                                           checkboxGroupInput("mlrCustom", 
+                                                              "Variables Used in Model:",
+                                                              c("Square Feet" = "area",
+                                                                "Bedrooms" = "bedrooms",
+                                                                "Bathrooms" = "bathrooms",
+                                                                "Stories" = "stories",
+                                                                "Mainroad" = "mainroad",
+                                                                "Guestroom" = "guestroom",
+                                                                "Basement" = "basement",
+                                                                "HotWaterHeating" = "hotwaterheating",
+                                                                "Airconditioning" = "airconditioning",
+                                                                "Parking" = "parking",
+                                                                "PreferredArea" = "prefarea",
+                                                                "FurnishingStatus" = "furnishingstatus"),
+                                                              selected = "area")),
+                                           checkboxInput("mlrInteractions",
+                                                         "Interactions among all variables considered"),
+                                           numericInput("mlrCvFold",
+                                                        label="Number of Folds for Cross Validation",
+                                                        value=5, min=1, max=10,
+                                                        step=1),
+                                           h4(strong("Bagged Tree Regression Model"), style = "color:red;"),
+                                           radioButtons("treeVariables", 
+                                                        "What Variables Should be Included?",
+                                                        c("All Variables" = "all",
+                                                          "Custom Subset" = "custom")),
+                                           conditionalPanel(condition = "input.treeVariables.includes('custom')",
+                                                            checkboxGroupInput("treeCustom", 
+                                                                               "Variables Used in Model:",
+                                                                               c("Square Feet" = "area",
+                                                                                 "Bedrooms" = "bedrooms",
+                                                                                 "Bathrooms" = "bathrooms",
+                                                                                 "Stories" = "stories",
+                                                                                 "Mainroad" = "mainroad",
+                                                                                 "Guestroom" = "guestroom",
+                                                                                 "Basement" = "basement",
+                                                                                 "HotWaterHeating" = "hotwaterheating",
+                                                                                 "Airconditioning" = "airconditioning",
+                                                                                 "Parking" = "parking",
+                                                                                 "PreferredArea" = "prefarea",
+                                                                                 "FurnishingStatus" = "furnishingstatus"),
+                                                                               selected = "area")),
+                                           numericInput("treeCvFold",
+                                                        label="Number of Folds for Cross Validation",
+                                                        value=5, min=1, max=10,
+                                                        step=1),
+                                           numericInput("treeCvRepeats",
+                                                        label="Number of Repeats for Cross Validation",
+                                                        value=3, min=1, max=10,
+                                                        step=1),
+                                           h4(strong("Random Forest Model"), style = "color:red;"),
+                                           radioButtons("forestVariables", 
+                                                        "What Variables Should be Included?",
+                                                        c("All Variables" = "all",
+                                                          "Custom Subset" = "custom")),
+                                           conditionalPanel(condition = "input.forestVariables.includes('custom')",
+                                                            checkboxGroupInput("forestCustom", 
+                                                                               "Variables Used in Model:",
+                                                                               c("Square Feet" = "area",
+                                                                                 "Bedrooms" = "bedrooms",
+                                                                                 "Bathrooms" = "bathrooms",
+                                                                                 "Stories" = "stories",
+                                                                                 "Mainroad" = "mainroad",
+                                                                                 "Guestroom" = "guestroom",
+                                                                                 "Basement" = "basement",
+                                                                                 "HotWaterHeating" = "hotwaterheating",
+                                                                                 "Airconditioning" = "airconditioning",
+                                                                                 "Parking" = "parking",
+                                                                                 "PreferredArea" = "prefarea",
+                                                                                 "FurnishingStatus" = "furnishingstatus"),
+                                                                               selected = "area")),
+                                           numericInput("forestCvFold",
+                                                        label="Number of Folds for Cross Validation",
+                                                        value=5, min=1, max=10,
+                                                        step=1),
+                                           numericInput("forestCvRepeats",
+                                                        label="Number of Repeats for Cross Validation",
+                                                        value=3, min=1, max=10,
+                                                        step=1),
+                                           br(),
+                                           actionButton("runModels", strong("Run Models"),style = "color: #35a7e8;")
+                                         ),
+                                         mainPanel(
+                                           verbatimTextOutput(outputId = "modelSummary"),
+                                           verbatimTextOutput(outputId = "vars")
+                                         )
+                                       )
+                              ),
+                              tabPanel("Prediction",
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           
+                                         ),
+                                         mainPanel(
+                                           
+                                         )
+                                       )
                               )
-                            )
+                            )  
                    ),
                    tabPanel("Data",
                             sidebarLayout(
@@ -55,8 +177,8 @@ shinyUI(navbarPage("Navigation Tabs",
                                 
                               )
                             )
-                   )
-        )
+                   ),
+        fluid=TRUE)
 )
 
 
